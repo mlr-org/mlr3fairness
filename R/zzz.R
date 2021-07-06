bc7e00b572c2e630e235b0831c343fefa21041eb
pfistfl <pfistererf@googlemail.com>
2021-07-06 19:04:01 +0200

    Add measures

--- R/zzz.R
+++ R/zzz.R
@@ -18,4 +18,8 @@
   x$task_col_roles$classif = union(x$task_col_roles$classif, "pta")
   x$task_col_roles$regr = union(x$task_col_roles$regr, "pta")
   # TODO: also do this for survival task in mlr3proba
+
+  x = getFromNamespace("mlr_measures", ns = "mlr3")
+  x$add("fairness", MeasureFairness)
+
 }
