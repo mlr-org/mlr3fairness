0e981185199c7ea09f85d6b9c53ef9a41fd0fa1b
Michel Lang <michellang@gmail.com>
2021-08-04 15:25:26 +0200

    update tic.R

--- tic.R
+++ tic.R
@@ -3,7 +3,6 @@ do_package_checks(error_on = "warning")
 get_stage("install") %>%
   # install ranger for README
   add_step(step_install_cran("ranger")) %>%
-  add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
