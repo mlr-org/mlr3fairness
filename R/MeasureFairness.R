0ebe98b912320d73b6ad63f48686f747466efd0e
pfistfl <pfistererf@googlemail.com>
2021-10-22 16:05:40 +0200

    Update paper

--- R/MeasureFairness.R
+++ R/MeasureFairness.R
@@ -116,7 +116,7 @@ MeasureFairnessComposite = R6::R6Class("MeasureFairnessComposite", inherit = Mea
     #'   Should the measure be minimized? Defaults to `TRUE`.
     #' @param range (`numeric`)\cr
     #'   Range of the resulting measure. Defaults to `c(-Inf, Inf)`.
-    initialize = function(id = NULL, measures, aggfun = function(x) mean(x, na.rm = TRUE),
+    initialize = function(id = NULL, measures, aggfun = function(x) sum(x, na.rm = TRUE),
       operation = groupdiff_absdiff, minimize = TRUE, range = c(-Inf, Inf)) {
 
       if (all(map(measures, class) == "character")) {
