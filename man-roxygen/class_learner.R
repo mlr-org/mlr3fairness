2bd6b6a9e07d3032dd5705466320c5691700a344
pfistfl <pfistererf@googlemail.com>
2021-12-07 12:46:27 +0100

    update learners

--- man-roxygen/class_learner.R
+++ man-roxygen/class_learner.R
@@ -11,16 +11,6 @@ lrn = mlr3::lrn(id)
 #' mlr_learners$get("<%= id %>")
 #' lrn("<%= id %>")
 #' ```
-#' @section Traits:
-#'
-#' * Packages: <%=mlr3misc::str_collapse(lrn$packages)%>
-#' * Predict Types: <%=mlr3misc::str_collapse(lrn$predict_types)%>
-#' * Feature Types: <%=mlr3misc::str_collapse(lrn$feature_types)%>
-#' * Properties: <%=mlr3misc::str_collapse(lrn$properties)%>
-#'
-#' @family fairness_learners
-#' @keywords internal
-<<<<<<< HEAD
 #' 
 #' @section Meta Information:
 #' `r mlr3misc::rd_info(mlr3::lrn("<%= id %>"))`
@@ -29,5 +19,6 @@ lrn = mlr3::lrn(id)
 #' @section Parameters:
 #' `r mlr3misc::rd_info(mlr3::lrn("<%= id %>")$param_set)`
 #' @md
-=======
->>>>>>> origin
+#' 
+#' @family fairness_learners
+#' @keywords internal
