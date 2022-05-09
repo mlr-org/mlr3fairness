c676a843021e2e282a46a669b8bd4cbe9c3fce9b
pfistfl <pfistererf@googlemail.com>
2022-05-09 15:41:20 +0200

    minor edge case tests

--- tests/testthat/test_pipeop_reweighing.R
+++ tests/testthat/test_pipeop_reweighing.R
@@ -88,4 +88,55 @@ test_that("reweighing int to char conversion", {
 })
 
 
+test_that("Edge cases", {
+  skip("Tested locally")
+  skip_on_cran()
+  skip_if_not_installed("mlr3oml")
+  skip_if_not_installed("mlr3pipelines")
+  library(mlr3)
+  library(mlr3pipelines)
+  library(mlr3fairness)
+  library(mlr3oml)
+  task = tsk("oml", task_id = 317599)
+  task$col_roles$pta = "x2"
+  l = as_learner(po("explicit_pta") %>>% po("reweighing_os") %>>% lrn("classif.rpart"))
+  l$train(task)
+  expect_true(!is.null(l$model))
+})
+
+test_that("Edge cases", {
+  skip("Tested locally")
+  skip_on_cran()
+  skip_if_not_installed("mlr3oml")
+  skip_if_not_installed("mlr3pipelines")
+  library(mlr3)
+  library(mlr3pipelines)
+  library(mlr3fairness)
+  library(mlr3oml)
+  task = tsk("oml", task_id = 317599)
+  task$col_roles$pta = "x2"
+  l = as_learner(po("explicit_pta") %>>% po("reweighing_os") %>>% lrn("classif.rpart"))
+  l$train(task)
+  expect_true(!is.null(l$model))
+})
+
+test_that("Edge cases", {
+  skip("Tested locally")
+  skip_on_cran()
+  skip_if_not_installed("mlr3oml")
+  skip_if_not_installed("mlr3pipelines")
+  library(mlr3)
+  library(mlr3pipelines)
+  library(mlr3fairness)
+  library(mlr3oml)
+  task = tsk("oml", task_id = 317599)
+  task$col_roles$pta = "x2"
+  l = lrn("classif.fairfgrrm")
+  r = resample(task, l, rsmp("cv", folds = 3L))
+  r$aggregate(msr("fairness.acc"))
+
+  l = as_learner(po("explicit_pta") %>>% lrn("classif.fairfgrrm"))
+  l
+
+})
 
